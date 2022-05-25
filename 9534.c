10a8360865787cc19ffa7fce2b7541f5e323abdd
sekulya <52187751+sekulya@users.noreply.github.com>
2022-05-25 19:27:02 +0800

    Create 9534.c
    练习题一
    输入并运行以下程序，分析程序的运行结果。程序代码
    #include "stdio.h" void mainO
    {intx=0165,a=322,b=-1; char c; c=a;
    printf("x1=%d,x2=%x,",x,x); printf("c=%c\n",c);
    printf("a:%d,%o,%x\n",a,a,a);
    printf("b:%d,%u,%x,%X\n",b,b,b,b);}
    分析
    ①给变量x赋初值使用八进制。
    ②输出变量x，用了两种格式，十进制和十六进制。
    ③将变量a的值赋给c，因为两个变量的类型不一样，分配的存储空间的长度也不同，整型长度为4字节，字符型为1字节，只把a的最低字节的内容赋给c，所以结果为大写字母B。
    ④输出变量a，分别用了十进制、八进制和十六进制。
    ⑤输出变量b，分别用了十进制、无符号十进制和十六进制(注意，小写x和大写X的区别)。
    
    输入输出说明:
    输入:无
    输出:见程序运行输出界面

---
+++ 9534.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+void main()
+{
+    int x=0165,a=322,b=-1;
+    char c;
+    c=a;
+    printf("x1=%d,x2=%x,",x,x);
+    printf("c=%c\n",c);
+    printf("a:%d,%o,%x\n",a,a,a);
+    printf("b:%d,%u,%x,%X\n",b,b,b,b);
+}
