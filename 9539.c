286498715e92e2b38e4372c93a7d7d522932b426
sekulya <52187751+sekulya@users.noreply.github.com>
2022-06-21 17:14:31 +0800

    9539.c
    题目内容:
    自测练习一
    输入并运行以下程序，分析程序的运行结果。程序代码
    #include<stdio.h> void main()
    { int a=5; double x;
    printf("%d %d\n",sizeof(int),sizeof(a));
    printf("%d %d\n",sizeof(double),sizeof(x)); printf("%d %d\n",sizeof(float),sizeof(char));
    
    输入输出说明:
    输入:无
    输出:见程序运行结果

---
+++ 9539.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main()
+{
+    int a=5; double x;
+    printf("%d %d\n",sizeof(int),sizeof(a));
+    printf("%d %d\n",sizeof(double),sizeof(x));
+    printf("%d %d\n",sizeof(float),sizeof(char));
+}
