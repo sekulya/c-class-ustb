0cfe063b78b1523967896916528821b81ed9a7ec
sekulya <52187751+sekulya@users.noreply.github.com>
2022-08-05 12:25:00 +0800

    Create 9678.c
    自测练习二
    设整型变量a、b、c分别存放从键盘输入的3个整数。编写C程序，按从大到小排列这了个数，使a
    成为最大值，c成为最小值，并且按序输出这了个数
    输入输出说明：
    输入：三个整数，以英文符号”，"分开
    输出：从大到小输出三个整数，以英文符号“，"分开

---
+++ 9678.c
@@ -0,0 +1,26 @@
+#include "stdio.h"
+void main()
+{
+    int a,b,c,t;
+    scanf("%d,%d,%d",&a,&b,&c);
+    if(a<c)
+    {
+        t=a;
+        a=c;
+        c=t;
+        
+    }
+    if (a<b)
+    {
+        t=a;
+        a=b;
+        b=t;
+    }
+    if (b<c)
+    {
+        t=b;
+        b=c;
+        c=t;
+    }
+    printf("%d,%d,%d",a,b,c);
+}
