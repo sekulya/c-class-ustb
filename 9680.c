ef4f2a0e8b06c35dd483d088239a3fa61af3d067
sekulya <52187751+sekulya@users.noreply.github.com>
2022-08-05 12:27:31 +0800

    Create 9680.c
    題目內容：
    自测练习三
    从键盘输入一个小于六位的正整数，判断它是几位数，并按照相反的顺序输出各位上的数字，
    例如输入1234，输出为4321。
    输入输出说明：
    输入：正整数，如“123”
    输出：逆序后的结果，如“321"

---
+++ 9680.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+void main()
+{
+    int n,x;
+    scanf("%d",&n);
+    x=n%7;
+    switch(x)
+    {
+        case 0:printf("%d天后是星期日\n",n);break;
+        case 1:printf("%d天后是星期一\n",n);break;
+        case 2:printf("%d天后是星期二\n",n);break;
+        case 3:printf("%d天后是星期三\n",n);break;
+        case 4:printf("%d天后是星期四\n",n);break;
+        case 5:printf("%d天后是星期五\n",n);break;
+        case 6:printf("%d天后是星期六\n",n);
+        
+    }
+}
