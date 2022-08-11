5c10aee5ac7c523563cfafe3e1f24b4904b31bb2
sekulya <52187751+sekulya@users.noreply.github.com>
2022-08-11 11:42:19 +0800

    Create 10162.c
    练习题二 (2)
    编写用“选择法”对N个字符进行排序的函数，将输入的10个字符按从小到大顺序排序并输出结果。
    选择法排序的算法参见主教材例5-5。
    程序代码（请修改）
    #include
    "stdio.h"
    void main( )
    { int i, j, ti
    char str[10];
    for (i=0; i<10 1491032497192185.jpg
    scanf("&c", &stril):
    for (j=1; j<10; j++)
    /*冒泡法排序*/
    for (i=0; i<10-j; i++)
    if (str[il›str{i+ll)
    { t=strlil;
    stril=stri+ll; strli+ll=t; !
    for (i=0; i<10; i++)
    printf("&c", strli]);
    printf("In"):
    输入输出说明：
    输入：连续的10个字符

---
+++ 10162.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+void main()
+{ int i,j,t; char str[10];
+    for (i=0;i<10;i++)
+       scanf("%c",&str[i]);
+     for (j=1;j<10;j++)
+     for( i=0;i<10-j;i++)
+     if (str[i]>str[i+1])
+        {t=str[i];str[i]=str[i+1];str[i+1]=t;}
+    for (i=0;i<10;i++)
+        printf("%c",str[i]);
+    printf("\n");
+} 
